e5bead66cd79682c6378ec056db2dfcd1e7e2c76
Montanaro02 <100145766+Montanaro02@users.noreply.github.com>
2022-04-01 17:40:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+    int n=0;
+    int prossimonumero;
+    cin>>n;
+    if(n>=2){
+        int primonumero=0;
+        int secondonumero=1;
+        cout<<secondonumero<<"\n";
+        for(int i=n; i>0; i--){
+            prossimonumero=primonumero+secondonumero;
+            primonumero=secondonumero;
+            secondonumero=prossimonumero;
+            cout<<prossimonumero<<"\n";
+        }
+    }else{
+        cout<<"errore";
+    }
+    return 0;
 }
